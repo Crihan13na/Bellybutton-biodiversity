3a470dd2eb756def3c17429a9c50b4ee0bf5d530
Crihan13na <crihan.anastasia@gmail.com>
2020-03-27 22:54:17 -0500

    bonus question works okay

--- HM/StarterCode/Belly_Button_Biodiversity/static/js/a.js
+++ HM/StarterCode/Belly_Button_Biodiversity/static/js/a.js
@@ -1,247 +1,109 @@
-console.log("Hello");
 
-init();
+  
 
-// ================================
-function init() {
-    
-    var selector = d3.select('#selDataset');
 
-    d3.json('samples.json').then(data => {
-        
-        var sampleNames = data.names;
-
-        sampleNames.forEach(sample => {
-           
-            selector
-                .append('option')
-                .text(sample)
-                .property('value',sample);
-        });
-
-        var firstSample = sampleNames[0];
-        buildCharts(firstSample);
-        buildMetadata(firstSample);
-        console.log(firstSample);
-    });
-};
-
-function optionChanged(newSample) {
-    buildCharts(newSample);
-    buildMetadata(newSample);
-    console.log(newSample);
-}
-
-function buildMetadata(sample) {
-    d3.json('samples.json').then(data => {
-        var metadata = data.metadata;
-
-        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-        var result = resultArray[0];
-        var panel = d3.select('#sample-metadata');
-        panel.html('');
-
-        Object.entries(result).forEach(([key, value]) => {
-            panel.append('h6').text(`${key.toUpperCase()}: ${value}`);
-        });
-
-        // buildGauge(reesult.wfreq);
-    });
-};
-
-function buildCharts(samples) {
-    d3.json('samples.json').then(data => {
-        var samples = data.samples;
-        var resultArray = samples.filter(sampleObj => sampleObj.id == samples);
-        var result = resultArray[0];
-        console.log("RESULT:");
-        console.log(result);
-        //error somewhere here!!!
-        //"samples" is the key -list of dictionary"
-        //"otu_labels" is the key - Array
-        //"sample_values" is the key -Array
-        var otu_ids = result(otu_ids);
-        console.log(otu_ids);
-        var otu_labels = result.otu_labels;
-        console.log(otu_labels);
-        var sample_values = result.sample_values;
-        console.log(sample_values);
-
-        var bubbleLayout = {
-            title: 'Vactera Cultures Per Sample',
-            margin: {t: 0 },
-            hovermode: 'closest',
-            xaxis: { title: 'OTU ID'},
-            margin: { t: 30}
-        };
-        var bubbleData = [
-            {
-                x: otu_ids,
-                y: sample_values,
-                text: otu_labels,
-                mode: 'markers',
-                marker: {
-                    size: sample_values,
-                    color: otu_ids,
-                    colorscale: 'Earth'
-                }
+  
+  // BONUS: Build the Gauge Chart
+  // buildGauge(data.WFREQ);
+  function buildGauge(sample) {
+    var url = "/metadata/" + sample;
+    d3.json(url).then(function(response) {
+      console.log(response.WFREQ);
+      var wfreq = response.WFREQ;
+      // Enter the washing frequency between 0 and 180
+      var level = parseFloat(wfreq) * 20;
+      // Trig to calc meter point
+      var degrees = 180 - level;
+      var radius = 0.5;
+      var radians = (degrees * Math.PI) / 180;
+      var x = radius * Math.cos(radians);
+      var y = radius * Math.sin(radians);
+  
+      // Path: may have to change to create a better triangle
+      var mainPath = "M -.0 -0.05 L .0 0.05 L ";
+      var pathX = String(x);
+      var space = " ";
+      var pathY = String(y);
+      var pathEnd = " Z";
+      var path = mainPath.concat(pathX, space, pathY, pathEnd);
+  
+      var data = [
+        {
+          type: "scatter",
+          x: [0],
+          y: [0],
+          marker: { size: 12, color: "850000" },
+          showlegend: false,
+          name: "Freq",
+          text: level,
+          hoverinfo: "text+name"
+        },
+        {
+          values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+          rotation: 90,
+          text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+          textinfo: "text",
+          textposition: "inside",
+          marker: {
+            colors: [
+              "rgba(0, 105, 11, .5)",
+              "rgba(10, 120, 22, .5)",
+              "rgba(14, 127, 0, .5)",
+              "rgba(110, 154, 22, .5)",
+              "rgba(170, 202, 42, .5)",
+              "rgba(202, 209, 95, .5)",
+              "rgba(210, 206, 145, .5)",
+              "rgba(232, 226, 202, .5)",
+              "rgba(240, 230, 215, .5)",
+              "rgba(255, 255, 255, 0)"
+            ]
+          },
+          labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+          hoverinfo: "label",
+          hole: 0.5,
+          type: "pie",
+          showlegend: false
+        }
+      ];
+  
+      var layout = {
+        shapes: [
+          {
+            type: "path",
+            path: path,
+            fillcolor: "850000",
+            line: {
+              color: "850000"
             }
-        ];
-
-        Plotly.newPlot('bubble',bubbleData,bubbleLayout);
+          }
+        ],
+        margin: {
+          l: 10,
+          r: 10,
+          b: 10,
+          t: 200,
+          pad: 4
+        },
+        title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+        height: 600,
+        width: 600,
+        xaxis: {
+          zeroline: false,
+          showticklabels: false,
+          showgrid: false,
+          range: [-1, 1]
+        },
+        yaxis: {
+          zeroline: false,
+          showticklabels: false,
+          showgrid: false,
+          range: [-1, 1]
+        }
+      }
+        var GAUGE = document.getElementById("gauge");
+        Plotly.newPlot(GAUGE, data, layout);
     });
-
-
-    // Horizonatl bar chart
-
-
-}
-  
-  
-
-
-//   // Sort the data by Greek search results
-// var sortedByGreekSearch = data.sort((a, b) => b.greekSearchResults - a.greekSearchResults);
-
-// // Slice the first 10 objects for plotting
-// slicedData = sortedByGreekSearch.slice(0, 10);
-
-// // Reverse the array to accommodate Plotly's defaults
-// reversedData = slicedData.reverse();
-
-// // Trace1 for the Greek Data
-// var trace1 = {
-//   x: reversedData.map(object => object.greekSearchResults),
-//   y: reversedData.map(object => object.greekName),
-//   text: reversedData.map(object => object.greekName),
-//   name: "Greek",
-//   type: "bar",
-//   orientation: "h"
-// };
-
-// // data
-// var data = [trace1];
-
-// // Apply the group bar mode to the layout
-// var layout = {
-//   title: "Greek gods search results",
-//   margin: {
-//     l: 100,
-//     r: 100,
-//     t: 100,
-//     b: 100
-//   }
-// };
-
-// // Render the plot to the div tag with id "plot"
-// Plotly.newPlot("plot", data, layout);
-
-
-
-
-
-  
-  // // BONUS: Build the Gauge Chart
-  // // buildGauge(data.WFREQ);
-  // function buildGauge(sample) {
-  //   var url = "/metadata/" + sample;
-  //   d3.json(url).then(function(response) {
-  //     console.log(response.WFREQ);
-  //     var wfreq = response.WFREQ;
-  //     // Enter the washing frequency between 0 and 180
-  //     var level = parseFloat(wfreq) * 20;
-  //     // Trig to calc meter point
-  //     var degrees = 180 - level;
-  //     var radius = 0.5;
-  //     var radians = (degrees * Math.PI) / 180;
-  //     var x = radius * Math.cos(radians);
-  //     var y = radius * Math.sin(radians);
-  
-  //     // Path: may have to change to create a better triangle
-  //     var mainPath = "M -.0 -0.05 L .0 0.05 L ";
-  //     var pathX = String(x);
-  //     var space = " ";
-  //     var pathY = String(y);
-  //     var pathEnd = " Z";
-  //     var path = mainPath.concat(pathX, space, pathY, pathEnd);
-  
-  //     var data = [
-  //       {
-  //         type: "scatter",
-  //         x: [0],
-  //         y: [0],
-  //         marker: { size: 12, color: "850000" },
-  //         showlegend: false,
-  //         name: "Freq",
-  //         text: level,
-  //         hoverinfo: "text+name"
-  //       },
-  //       {
-  //         values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
-  //         rotation: 90,
-  //         text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
-  //         textinfo: "text",
-  //         textposition: "inside",
-  //         marker: {
-  //           colors: [
-  //             "rgba(0, 105, 11, .5)",
-  //             "rgba(10, 120, 22, .5)",
-  //             "rgba(14, 127, 0, .5)",
-  //             "rgba(110, 154, 22, .5)",
-  //             "rgba(170, 202, 42, .5)",
-  //             "rgba(202, 209, 95, .5)",
-  //             "rgba(210, 206, 145, .5)",
-  //             "rgba(232, 226, 202, .5)",
-  //             "rgba(240, 230, 215, .5)",
-  //             "rgba(255, 255, 255, 0)"
-  //           ]
-  //         },
-  //         labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
-  //         hoverinfo: "label",
-  //         hole: 0.5,
-  //         type: "pie",
-  //         showlegend: false
-  //       }
-  //     ];
-  
-  //     var layout = {
-  //       shapes: [
-  //         {
-  //           type: "path",
-  //           path: path,
-  //           fillcolor: "850000",
-  //           line: {
-  //             color: "850000"
-  //           }
-  //         }
-  //       ],
-  //       margin: {
-  //         l: 10,
-  //         r: 10,
-  //         b: 10,
-  //         t: 200,
-  //         pad: 4
-  //       },
-  //       title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
-  //       height: 600,
-  //       width: 600,
-  //       xaxis: {
-  //         zeroline: false,
-  //         showticklabels: false,
-  //         showgrid: false,
-  //         range: [-1, 1]
-  //       },
-  //       yaxis: {
-  //         zeroline: false,
-  //         showticklabels: false,
-  //         showgrid: false,
-  //         range: [-1, 1]
-  //       }
-  //     }
-  //       var GAUGE = document.getElementById("gauge");
-  //       Plotly.newPlot(GAUGE, data, layout);
-  //   });
-  // }
+  }
   
   // function buildCharts(sample) {
   //   var url = "/samples/" + sample;
--- HM/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ HM/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -4,24 +4,27 @@ function InitDashboard()
 {
 
     //console.log("Initializing Dashboard");
-
+    // Reference dropdown menu and store in a variable called 'selector'
     var selector = d3.select("#selDataset");
-
+    // Fetch the JSON data
     d3.json("samples.json").then((data) => {
          
         //console.log(data);
-
+        // Create variable to hold all the names
         var sampleNames = data.names;
 
         //console.log(data.names);
-
+        // Modify the selector for each name
         sampleNames.forEach((sampleId) => 
         {
+            // Add 'option' tag
             selector.append("option")
+            // Text displayed on user interface
             .text(sampleId)
+            // Add 'value' attribute to each tag and assign 'sample' to it
             .property("value", sampleId);
         });
-        
+        // Pick first name to be the default data for our graphs
         var sampleId = sampleNames[0];
 
         DrawBubblechart(sampleId);
@@ -36,6 +39,7 @@ function InitDashboard()
  
  function optionChanged(newSampleId) 
  {
+     // Function to build the tables with user input
      //console.log(`User selected ${newSampleId}`);
  
      DrawBubblechart(newSampleId);
@@ -48,13 +52,17 @@ function InitDashboard()
  
  function ShowMetadata(sampleId)
 {
+    // Create function to populate 'Demographic Info' table
     //console.log(`calling ShowMetadata(${sampleId})`);
-
+    // Fetch the JSON data
     d3.json("samples.json").then((data) => {
+        // Create variable to hold all the metadata
         var metadata = data.metadata;
         var resultArray = metadata.filter(m => m.id == sampleId);
         var result = resultArray[0];
+        // Reference location to put metadata and place into variable 'panel'
         var Panel = d3.select(`#sample-metadata`);
+        // First empty out any data in that location
         Panel.html(``);
 
         Object.entries(result).forEach(([key, value]) => {
@@ -70,10 +78,12 @@ function InitDashboard()
 
 function DrawBargraph(sampleId)
 {
-    console.log(`calling DrawBargraph(${sampleId})`);
-     //reading data
+    //console.log(`calling DrawBargraph(${sampleId})`);
+     // Fetch JSON data
     d3.json("samples.json").then((data) => {
         var samples = data.samples;
+        // Within 'samples' from samples.json, find the id that 
+        //matches the user input (=sampleId) in drop down menu
         var resultArray = samples.filter(s => s.id == sampleId);
         var result = resultArray[0];
       
@@ -84,7 +94,7 @@ function DrawBargraph(sampleId)
         //console.log(` data for otu_labels ${otu_labels}`);
         var sample_values = result.sample_values;
         //console.log(` data for sample_values ${sample_values}`);
-
+        // Want bar chart of top 10, highest on top; need to slice and reverse
         yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
        // console.log(yticks);
 
@@ -161,9 +171,59 @@ function DrawBubblechart(sampleId)
 
 
 
-function DrawGauge(sampleId)
+function DrawGauge(sampleId) {
 {
-    console.log(`calling DrawGauge(${sampleId})`);
+    //console.log(`calling DrawGauge(${sampleId})`); 
+    d3.json('samples.json').then(data => {
+            var metadata = data.metadata;
+            var resultArray = metadata.filter(sampleObj => sampleObj.id == sampleId);
+            var result = resultArray[0];
+            var washFreq = result.wfreq;
+            console.log("GAUGE washFreq:", washFreq);
+            
+            var data = [
+                {
+                domain: { x: [0, 1], y: [0, 1]},
+                value: washFreq,
+                title: { text: "Hand Wash Freq", font: { size: 24 } },
+                type: "indicator",
+                mode: "gauge+number",
+                gauge: { 
+                    axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue" }, 
+                    bar: { color: "red" },
+                    bgcolor: "white",
+                    borderwidth: 2,
+                    bordercolor: "gray",
+                    steps: [
+                        { range: [0, 1], color: "#33cca6"},
+                        { range: [1, 2], color: "#33cccc" },
+                        { range: [2, 3], color: "#33a6cc" },
+                        { range: [3, 4], color: "#3380cc" },
+                        { range: [4, 5], color: "#3359cc" },
+                        { range: [5, 6], color: "#3333cc" },
+                        { range: [6, 7], color: "#5933cc" },
+                        { range: [7, 8], color: "#8033cc" },
+                        { range: [8, 9], color: "#a633cc" } 
+                    ],
+                }
+         }
+     ];
+
+     var layout = { 
+         width: 465, 
+         height: 400, 
+         margin: { t: 25, r: 25, l: 25, b: 25 },
+         //paper_bgcolor: "black",
+         font: {
+             color: "earth",
+             //family: "Arial"
+             }
+     };
+     Plotly.newPlot("gauge", data, layout);
+ });
+}
+    
+
 }
 
 
